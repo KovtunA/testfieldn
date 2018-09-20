b0624cabebf20e2bb92e61d37b9a4c169152bce9
KovtunA <42818685+KovtunA@users.noreply.github.com>
2018-09-20 17:06:38 +0300

    first commit

---
+++ testfieldn/testfieldn/Test.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace testfieldn
+{
+    [TestFixture()]
+    public class Test
+    {
+        static IWebDriver driver;
+
+        [Test()]
+        public void TestCase()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://saucelabs.com");
+            IWebElement header = driver.FindElement(By.Id("site-header"));
+            Assert.True(header.Displayed);
+            driver.Close();
+        }
+    }
+}
